2564fca8448e5eb9a7dfc16476f7b5a2dffaa10d
Cfun1 <15718354+Cfun1@users.noreply.github.com>
2024-10-06 12:35:36 -0400

    Add material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderModule } from './header/header.module';
 import { MainContentModule } from './main-content/main-content.module';
 import { FooterModule } from './footer/footer.module';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [AppComponent, NavbarComponent],
-	imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule, NgbModule, HeaderModule, MainContentModule, FooterModule],
+	imports: [BrowserModule, AppRoutingModule, NgbModule, HeaderModule, MainContentModule, FooterModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
